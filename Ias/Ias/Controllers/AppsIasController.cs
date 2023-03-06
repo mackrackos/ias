1da9dc3ca93e5b64abffcac7633d1a516a828791
Jorge Aguilar <jorge.bravo77@gmail.com>
2023-03-06 08:38:12 -0600

    Crear vistas de AppsController

--- Ias/Ias/Controllers/AppsIasController.cs
+++ Ias/Ias/Controllers/AppsIasController.cs
@@ -1,23 +1,64 @@
 using System.Linq;
 using System.Web.Mvc;
+using Google.Protobuf.WellKnownTypes;
 using Ias.Models;
 
 namespace Ias.Controllers
 {
     public class AppsIasController : Controller
     {
-        // GET
+        [HttpGet]
         public ActionResult Index()
         {
             var apps = new AppIas().GetApps();
             return View(apps);
         }
 
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            var apps = new AppIas().GetApps();
-            return View(apps.FirstOrDefault(a => a.Id == id));
+            var app = new AppIas().GetApps(id);
+            return View(app);
         }
-        
+
+        [HttpPost]
+        public ActionResult Edit(AppIas app)
+        {
+            var result = new AppIas().UpdateApps(app);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(AppIas app)
+        {
+            var result = new AppIas().InsertApps(app);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var app = new AppIas().GetApps(id);
+
+            return View(app);
+        }
+
+        [HttpPost]
+        public ActionResult DeletePost(int id)
+        {
+            var app = new AppIas().DeleteApps(id);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
